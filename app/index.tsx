699b36f9d23ba858c75ee40402e936c5f9feb1b7
Shimelis <shimelisgetahun07@gmail.com>
2025-09-22 08:22:10 -0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-wheat-200">
+      <Text className="text-3xl font-bold text-blue-500">
+        Welcome to NativeWind! .
+      </Text>
+    </View>
+  );
+}
